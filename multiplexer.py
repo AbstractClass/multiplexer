13b0011a6be5eb48f0ce17c7d1ec9f0932732b60
root <ctmacleod@myseneca.ca>
2018-09-04 00:46:09 -0400

    code actually works now

--- multiplexer.py
+++ multiplexer.py
@@ -1,3 +1,4 @@
+#!/usr/env/python3
 import argparse
 import logging
 import multiprocessing
@@ -22,14 +23,15 @@ parser.add_argument(
     help="Location of file containing the targets of the commands in the template.")
 
 parser.add_argument(
-    "cpus",
-    help="number of simultaneous process to use.  "
-         "Not specifying this will default to 2 or (however many cores you have) - 2, whatever is larger.")
-
-parser.add_argument(
     "--force",
     help="Ignore contents of save file.",
     action='store_true')
+
+parser.add_argument(
+    "--cpus",
+    help="number of simultaneous process to use.  "
+         "Not specifying this will default to 2 or (however many cores you have) - 2, whatever is larger.",
+    type=int)
 
 parser.add_argument(
     "--savefile",
@@ -42,13 +44,13 @@ args = parser.parse_args()
 class Multiplexer:
     def __init__(self, template, payloads, cpu_count, savefile="~/.multiplexer_progress"):
         self.template = template
-        self.payload = payloads
+        self.payloads = payloads
         self.savefile = savefile
         self.cpu_count = cpu_count
-        self.tasks = self.make_tasks(self.template, self.payload)
+        self.tasks = self.make_tasks(self.template, self.payloads)
         self.completed_tasks = []
 
-    def make_tasks(self, templates, payloads):
+    def make_tasks(self, template, payloads):
         """
         We have templates, we have paylaods, generate all possibilities and push into a list (grouped by template entry)
 
@@ -57,19 +59,19 @@ class Multiplexer:
         :return: list of all possibilities of templates and payloads
         """
         tasklist = []
-        for command in templates:
+        for command in template:
             for payload in payloads:
                 # This feels kinda hand-holdy, but I am keeping it for now
                 try:
-                    tasklist.append(command.format(payload))
+                    tasklist.append(command.format(*payload))
 
                 except IndexError:
                     log.error("Index Error in template entry: {}".format(command))
                     print("Index Error in template entry: {}".format(command))
                     input("Press any key to continue (or ctrl+c to exit)...")
-                    break
 
-            return tasklist
+
+        return tasklist
 
     def multiplex(self):
         """
@@ -78,13 +80,13 @@ class Multiplexer:
         """
         pool = multiprocessing.Pool(processes=self.cpu_count)
         try:
-            log.info("Attempting to run {} tasks over {} cores".format(self.tasks.count, self.cpu_count))
+            # log.info("Attempting to run {} tasks over {} cores".format(self.tasks.count, self.cpu_count))
             pool.map(self.spawn_process, self.tasks)
-            log.info("Completed multiprocessing operation.  Any failed tasks will be written to the save file.")
+            # log.info("Completed multiprocessing operation.  Any failed tasks will be written to the save file.")
 
         # Please tell me there is a better way to do this.
         except (KeyboardInterrupt, SystemExit):
-            log.info("Intercepted Keyboard Interrupt.  Cleaning up...")
+            # log.info("Intercepted Keyboard Interrupt.  Cleaning up...")
             remaining_tasks = [task for task in self.tasks if task not in self.completed_tasks]
             self.write_savefile(remaining_tasks)
             sys.exit(0)
@@ -99,8 +101,8 @@ class Multiplexer:
         Open savefile, read into list, run those tasks through the multiplexer
         :return: None
         """
-        self.tasks = open(self.savefile, 'r').read().splitlines()
-        log.info("Resuming operations based on savefile: {}".format(self.savefile))
+        self.tasks = open(os.path.expanduser(self.savefile), 'r').read().splitlines()
+        # log.info("Resuming operations based on savefile: {}".format(self.savefile))
         self.multiplex()
 
     def spawn_process(self, command):
@@ -111,14 +113,15 @@ class Multiplexer:
         :param command: string that represents a bash command (ie. "ls /home")
         :return: the subprocess retcode
         """
-        log.debug("Attempting: {}".format(command))
-        retcode = subprocess.run(command.split(' '))
+        # log.debug("Attempting: {}".format(command))
+        retcode = subprocess.run(command.split(' '), stdout=None, stderr=None)
 
         if retcode == 0:
-            log.info("Success: {}".format(command))
+            # log.info("Success: {}".format(command))
             self.completed_tasks.append(command)
         else:
-            log.error("Failed: {}".format(command))
+            pass
+            # log.error("Failed: {}".format(command))
         return retcode
 
     def write_savefile(self, uncompleted_tasks):
@@ -132,19 +135,26 @@ if __name__ == '__main__':
     read_into_list = lambda x: open(x, 'r').read().splitlines()
 
     commands = read_into_list(args.template)
-    payloads = read_into_list(args.payloads)
+    raw_payloads = read_into_list(args.payloads)
 
-    if not args.cpu_count:
+    payloads = [payload.strip().split('\t') for payload in raw_payloads]
+
+    if not args.cpus:
         if multiprocessing.cpu_count() <= 2:
-            cpu_count = 2
+            args.cpus = 2
 
         else:
-            cpu_count = multiprocessing.cpu_count() - 2
+            args.cpus = multiprocessing.cpu_count() - 2
 
-    mp = Multiplexer(commands, payloads, savefile=args.savefile)
+    print(args.cpus)
+    mp = Multiplexer(commands, payloads, args.cpus, savefile=args.savefile)
 
-    if args.force:
-        mp.multiplex()
+    if os.path.exists(os.path.expanduser(args.savefile)):
+        if args.force:
+            mp.multiplex()
+
+        else:
+            mp.resume()
 
     else:
-        mp.resume()
+        mp.multiplex()
