0583dec0e5b4e57085bae6b335b11335627bdc28
Connor <connor.macleod@hotmail.ca>
2019-02-19 16:02:30 -0500

    Housekeeping + Bugfixing

--- multiplexer_v2.0.py
+++ multiplexer_v2.0.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
-import tqdm
+#!/usr/env/python3
+from tqdm import tqdm
 from typing import IO
 import argparse
 import logging
-import pathos.multiprocessing as multiprocessing
-from pathos.helpers import mp as pathos_multiprocess
+import multiprocessing
 import os
 import subprocess
 import sys
@@ -45,125 +44,102 @@ parser.add_argument(
     "--savefile",
     type=argparse.FileType('r+'),
     help="Location file containing tasks to complete (default is '~/.multiplexer_progress').",
-    default=os.path.expanduser("~/.multiplexer_progress"))
+    default="~/.multiplexer_progress")
 
 args = parser.parse_args()
 
 class Multiplexer:
-    def __init__(self,
-                 template: IO[str],
-                 payloads: IO[str],
-                 cpu_count: int,
-                 savefile: IO[str],
-                 resume: bool = True,
-                 stdout: str = os.devnull,
-                 stderr: str = os.devnull) -> None:
-        self.resume = resume
-        self.template = template.readlines()
-        self.payloads = [payload.strip().split('\t') for payload in payloads]
+    def __init__(self, template: IO[str], payloads: IO[str], cpu_count: int, savefile: IO[str]) -> None:
+        self.template = template
+        self.payloads = payloads
         self.savefile = savefile
         self.cpu_count = cpu_count
         self.tasks = self.make_tasks()
-        manager = pathos_multiprocess.Manager()
-        self.completed_tasks = manager.list()
-        self.stdout = open(stdout, 'w')
-        self.stderr = open(stderr, 'w')
+        self.completed_tasks = []
 
-
-    def make_tasks(self, payloads=None) -> list:
+    def make_tasks(self) -> list:
         """
         We have templates, we have paylaods, generate all possibilities and push into a list (grouped by template entry)
         """
-        if not payloads:
-            payloads = self.payloads
-
         tasklist = []
         for command in self.template:
-            for payload in payloads:
+            for payload in self.payloads:
                 # This feels kinda hand-holdy, but I am keeping it for now
                 try:
-                    tasklist.append(command.strip().format(*payload))
+                    tasklist.append(command.format(*payload))
 
                 except IndexError:
                     log.error(f"Index Error in template entry: {command}")
                     print(f"Index Error in template entry: {command}")
                     input("Press any key to continue (or ctrl+c to exit)...")
 
-        unfinished_tasks = self.savefile.readlines()
-        if unfinished_tasks and self.resume:
-            resume_answer = input(
-                f"There are {len(unfinished_tasks)} unfinished jobs remaining, want to run them now? [Y]")
-            if not resume_answer or 'y' in resume_answer.lower():
-                tasklist = list(set(unfinished_tasks + tasklist))
-
         return tasklist
 
-    def spawn_process(self, command):
+    def spawn_process(self, command: str) -> subprocess.CompletedProcess:
         """
         Barely even needs to be a function honestly, just did it for logging and A e s t h e t i c.
         Run a command string as a separate process and return the retcode on completion.
         """
         # log.debug("Attempting: {}".format(command))
-        retcode = subprocess.run(command.split(' '), stdout=self.stdout, stderr=self.stderr)
-        if retcode.returncode == 0:
+        retcode = subprocess.run(command.split(' '), stdout=None, stderr=None)
+
+        if retcode == 0:
             # log.info("Success: {}".format(command))
             self.completed_tasks.append(command)
+            tqdm.write("Completed:", command)
         else:
-            tqdm.tqdm.write(f"Failed: {command}")
-            self.pbar.refresh()
+            tqdm.write("Failed:", command)
             pass
             # log.error("Failed: {}".format(command))
+        return retcode
 
     def multiplex(self) -> list:
         """
         Run self.tasks in parallel and record remaining tasks
         :return: tasks not completed
         """
+        pool = multiprocessing.Pool(processes=self.cpu_count)
 
-        tqdm.tqdm.write("Starting the Multiplexer!\n"
-              f"Running {len(self.tasks)} tasks over {self.cpu_count} cores.\n"
-              f"Savefile location is: {self.savefile.name}")
+        tqdm.write("Starting the Multiplexer! "
+              f"Running {len(self.tasks)} tasks over {self.cpu_count} cores. "
+              f"Savefile location is: {self.savefile}")
 
         try:
-            with multiprocessing.Pool(processes=self.cpu_count) as pool:
-                self.pbar = tqdm.tqdm(pool.imap(self.spawn_process, self.tasks), total=len(self.tasks), ascii=True)
-                list(self.pbar)
+            tqdm(pool.imap(self.spawn_process, self.tasks))
 
         # Please tell me there is a better way to do this.
-        except:
+        except (KeyboardInterrupt, SystemExit):
             # log.info("Intercepted Keyboard Interrupt.  Cleaning up...")
             remaining_tasks = [task for task in self.tasks if task not in self.completed_tasks]
             self.write_savefile(remaining_tasks)
             sys.exit(0)
 
         remaining_tasks = [task for task in self.tasks if task not in self.completed_tasks]
-
-        if remaining_tasks:
-            print(f"{len(remaining_tasks)} tasks unfinished and being written to savefile.")
-            self.write_savefile(remaining_tasks)
-
-        else:
-            print("All tasks completed successfully.")
-            self.savefile.truncate(0)
+        self.write_savefile(remaining_tasks)
 
         return remaining_tasks
+
+    def savefile_sanity_check(self) -> None:
+        for task in self.completed_tasks:
+            if task not in self.tasks:
+                tqdm.write("Looks like you are running a new job, savefile will be discarded upon completion.")
+                self.completed_tasks = []
+
+
+
+    def attempt_resume(self) -> None:
+        """
+        Open savefile, read into list, run those tasks through the multiplexer
+        :return: None
+        """
+        self.savefile_sanity_check()
+        self.tasks = [cmd for cmd in self.savefile]
+        # log.info("Resuming operations based on savefile: {}".format(self.savefile))
+        self.multiplex()
+
+
 
     def write_savefile(self, uncompleted_tasks):
         self.savefile.truncate(0)
         for task in uncompleted_tasks:
             self.savefile.write(task+'\n')
-
-if __name__ == '__main__':
-    # PEP8 be damned, lambdas exist and I will use them!
-    read_into_list = lambda x: open(x, 'r').read().splitlines()
-
-    commands = args.template
-    payloads = args.payloads
-
-    if not args.cpus:
-        cpu_count = multiprocessing.cpu_count()
-        args.cpus = 2 if cpu_count <= 2 else cpu_count - 2
-
-    mp = Multiplexer(commands, payloads, args.cpus, savefile=args.savefile, resume=not args.force)
-
-    mp.multiplex()
